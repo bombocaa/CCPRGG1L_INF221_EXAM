c778907023f2dc7d17b9fee559c54461efc8e118
Jasper Deguito <110919412+bombocaa@users.noreply.github.com>
2022-09-05 10:18:40 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    public static void main(String[] args){
+
+        App student = new App();
+
+        student.firstName= "Jasper Raul";
+        student.dateOfBirth = "06/14/03";
+        
+
+        student.sayMyStudentNumber = 2022100967;
+        student.sayMyStudentNumber();
+
+        student.sayMyEmailAddress = "deguitojz@students.national-u.edu.ph";
+        student.sayMyEmailAddress();
+
+        student.iAmAwesome = true;
+        student.iAmAwesome();
+
+
+
+
+    
+    }
+}
